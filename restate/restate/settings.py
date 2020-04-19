5584ac268ab3b884be3441abb7228faf3afe494d
Apoorva Ambulgekar <apoorvaflame@gmail.com>
2020-04-19 20:19:08 +0530

    Update settings.py

--- restate/restate/settings.py
+++ restate/restate/settings.py
@@ -150,5 +150,5 @@ MEDIA_URL='/media/'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='apoorvaflame@gmail.com'
-EMAIL_HOST_PASSWORD='Animax12#'
+EMAIL_HOST_PASSWORD='password'
 EMAIL_USE_TLS=True
